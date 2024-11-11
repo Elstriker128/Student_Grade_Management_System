abff0242d058e3f32254b048dce8f051444a51a2
Rugile Rankauskaite <rugile.rank@gmail.com>
2024-11-11 10:00:12 +0200

    Pridėti įvertinimų langai

---
+++ Student_Grade_Management_System/Controllers/MarksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Grade_Management_System.Controllers
+{
+    public class MarksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            // Code to add a new class
+            return View();
+        }
+        public IActionResult ClassMarks()
+        {
+            // Code to create a new schedule for a class
+            return View();
+        }
+        public IActionResult Graphic()
+        {
+            // Code to add a new class
+            return View();
+        }
+    }
+}
