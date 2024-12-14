1a2fa5c95898edfda8c7d79323a8a8b6cb522857
Elstriker128 <89691298+Elstriker128@users.noreply.github.com>
2024-12-15 00:11:48 +0200

    Changes to work out branch issues

--- Student_Grade_Management_System/obj/Debug/net7.0/Student_Grade_Management_System.AssemblyInfo.cs
+++ Student_Grade_Management_System/obj/Debug/net7.0/Student_Grade_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student_Grade_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ba31522c7864fc4ca8c16550d282504c0b51e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a51fb12fa53a72e16362a284ab9936cf81e0a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student_Grade_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student_Grade_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
