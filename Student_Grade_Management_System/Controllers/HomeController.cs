a0423f0b1ee414dc20e2832f7292e2c5b829637b
Elstriker128 <89691298+Elstriker128@users.noreply.github.com>
2024-12-15 20:28:37 +0200

    Added more session functionalities

--- Student_Grade_Management_System/Controllers/HomeController.cs
+++ Student_Grade_Management_System/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ namespace Student_Grade_Management_System.Controllers
     {
         private readonly SystemDbContext _context;
         private readonly ILogger<HomeController> _logger;
-        private readonly SystemDbContext _context;
 
         public HomeController(SystemDbContext context, ILogger<HomeController> logger)
         {
